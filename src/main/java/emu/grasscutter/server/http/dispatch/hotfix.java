5683fdb53d4f43777efec7e5fd45b6018ad72306
Deuteriunt. <1619167551@qq.com>
2024-08-30 16:04:13 +0800

    add hotfix

---
+++ src/main/java/emu/grasscutter/server/http/dispatch/hotfix.java
@@ -0,0 +1,30 @@
+package emu.grasscutter.server.http.dispatch;
+
+public class hotfix {
+
+    public static class ResVersionConfig {
+        public int version = 26458901;
+        public String md5 =
+                "{\"remoteName\": \"res_versions_external\", \"md5\": \"5f1ba25c0da41520d3d3057b1a32fd29\", \"hash\": \"ed66319e5bf7ec7e\", \"fileSize\": 153828}\r\n{\"remoteName\": \"res_versions_medium\", \"md5\": \"5907101204778f6b2f4bf3d8e4251d42\", \"hash\": \"5369f779aa07021d\", \"fileSize\": 8988}\r\n{\"remoteName\": \"res_versions_streaming\", \"md5\": \"bb50a573cbe1c9a5360578f006016a78\", \"hash\": \"a12089af1c385746\", \"fileSize\": 1833}\r\n{\"remoteName\": \"base_revision\", \"md5\": \"149cad27b543e345df504a496949ec7d\", \"hash\": \"8bb70126e5cd8ba0\", \"fileSize\": 19}\r\n{\"remoteName\": \"patch_node_versions\", \"md5\": \"68df81e80dd3e13bb8b55d4794b9948e\", \"hash\": \"38e59d29a11bdeaf\", \"fileSize\": 1751}\r\n{\"remoteName\": \"audio_diff_versions_26458901-24822834\", \"md5\": \"8d3b4e9a18b8ef5932287fc68106e63e\", \"hash\": \"6d6877dd57e08f73\", \"fileSize\": 36439}";
+        public String releaseTotalSize = "0";
+        public String versionSuffix = "befdda25ff";
+        public String branch = "5.0_live";
+    }
+
+    public static class Resource {
+        public String resourceUrl = "https://autopatchhk.yuanshen.com/client_game_res/5.0_live";
+        public String dataUrl = "https://autopatchhk.yuanshen.com/client_design_data/5.0_live";
+        public String resourceUrlBak = "5.0_live";
+        public int clientDataVersion = 26487341;
+        public int clientSilenceDataVersion = 26368837;
+        public String clientDataMd5 =
+                "{\"remoteName\": \"data_versions\", \"md5\": \"88a0d1f6825ec3b6aaf9ea39a02f78da\", \"hash\": \"a72baf3b5c76f0ac\", \"fileSize\": 68545}\r\n{\"remoteName\": \"data_versions_medium\", \"md5\": \"9429b4e9dd8cbdaf19c41ff05f18b384\", \"hash\": \"a79950c775cf1630\", \"fileSize\": 6662}";
+        public String clientSilenceDataMd5 =
+                "{\"remoteName\": \"data_versions\", \"md5\": \"8ae3d12ddeffa27349ab306ce05ec0b7\", \"hash\": \"ef8cb53633584c7a\", \"fileSize\": 522}";
+        public ResVersionConfig resVersionConfig = new ResVersionConfig();
+        public String clientVersionSuffix = "57a90bbd52";
+        public String clientSilenceVersionSuffix = "0af120923b";
+        public String nextResourceUrl = "https://autopatchhk.yuanshen.com/client_game_res/5.0_live";
+        public ResVersionConfig nextResVersionConfig = new ResVersionConfig();
+    }
+}
